3645588f6a1d01e5071445a777f808ef1fa38719
Dorab Patel <dorabpatel@gmail.com>
2021-01-13 09:41:57 -0800

    Updated emacs init file.

--- src/.emacs.d/init.el
+++ src/.emacs.d/init.el
@@ -4,30 +4,29 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-;; Periodically run (list-packages) to update.
-;; (package-install-selected-packages) to do initial install.
+;; On initial install, do the following:
+;; Note that the initial startup will complain about exec-path-from-shell-initialize
+;; Ignore that.
+;; M-x package-refresh-contents
+;; M-x package-install-selected-packages
+;; For some reason, the list of packages will not include org, so have to install manually.
+;; M-x list-packages
+;; Then re-search for "available *org" in the pacakge list buffer
+;; Install the org package manually ("i", then "x", then "y").
+;; Then restart emacs.
+
+;; Periodically run (list-packages) to update packages.
+
 ;; Currently selected packages are in the var package-selected-packages
 
-(defun dorab-after-init-hook ()
-  ;; do stuff after package initialization
-  (exec-path-from-shell-initialize)
-  )
-(add-hook 'after-init-hook 'dorab-after-init-hook)
-
-;; for Infobright
-(defun sql-mysqlib ()
-  (interactive)
-  (let ((sql-mysql-program "mysql") ;; or mysql-ib if infobright installed
-	(sql-port 3307))
-    (call-interactively 'sql-mysql)))
-;; for MySQL
-(setq sql-port 3306)
-(setq sql-mysql-program "mysql")
 
 (put 'narrow-to-region 'disabled nil)
 
-;; Not sure I now want this in all text mode buffers.
-; (add-hook 'text-mode-hook (lambda () (auto-fill-mode 1)))
+(add-to-list 'auto-mode-alist (cons "\\.adoc\\'" 'adoc-mode))
+
+(add-hook 'after-init-hook 'exec-path-from-shell-initialize)
+
+(add-hook 'text-mode-hook (lambda () (auto-fill-mode 1)))
 
 (add-hook 'java-mode-hook (lambda ()
 			    (setq c-basic-offset 2)
@@ -38,9 +37,6 @@
 			   (setq ess-first-continued-statement-offset 2)
 			   (setq ess-continued-statement-offset 0)))
 
-;; Remove after I send in the patch for git.el
-(load "~/.emacs.d/git.el")
-
 ;;;;;;;;;;;;;;;;
 ;; use regexp versions
 (global-set-key "\C-s" 'isearch-forward-regexp)
@@ -50,36 +46,56 @@
 (global-set-key "\C-\\" 'compile)
 
 ;;;;;;;; org mode stuff
+(require 'ob-clojure)
 (require 'org-crypt)
 (org-crypt-use-before-save-magic)	;encrypt on save
 (setq org-tags-exclude-from-inheritance (quote ("crypt")))
 (setq org-crypt-key nil)		;use symmetric encryption
+
+;; Fix indentation of some common macros
+;;  (define-clojure-indent
+;;    (for-all 1)
+;;    (defroutes 'defun)
+;;    (GET 2)
+;;    (POST 2)
+;;    (PUT 2)
+;;    (DELETE 2)
+;;    (HEAD 2)
+;;    (ANY 2)
+;;    (context 2)
+;;    (reporting 1))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-clojure-cli-global-options "-A:dev:test")
+ '(cider-invert-insert-eval-p t)
+ '(cider-repl-use-content-types t)
+ '(cider-switch-to-repl-on-insert nil)
  '(ido-mode t nil (ido))
+ '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(line-move-visual nil)
  '(menu-bar-mode nil)
- '(org-log-done (quote time))
  '(package-archives
-   (quote
-    (("melpa-stable" . "http://stable.melpa.org/packages/")
+   '(("melpa-stable" . "http://stable.melpa.org/packages/")
      ("gnu" . "http://elpa.gnu.org/packages/")
-     ("org" . "http://orgmode.org/elpa/"))))
- '(package-pinned-packages (quote ((org . "org"))))
+     ("org" . "http://orgmode.org/elpa/")))
+ '(package-pinned-packages '((org . "org") (cider . "melpa-stable")))
  '(package-selected-packages
-   (quote
-    (markdown-mode
-     terraform-mode
-     yaml-mode
-     org
-     exec-path-from-shell
-     cider)))
+   '(adoc-mode cider exec-path-from-shell lsp-mode magit markdown-mode org sql-indent terraform-mode yaml-mode))
+ '(safe-local-variable-values
+   '((cider-shadow-cljs-global-options . "-A:dev")
+     (cider-shadow-global-options . "-A:dev:java9")
+     (cider-preferred-build-tool . shadow-cljs)
+     (cider-shadow-default-options . ":app")
+     (cider-default-cljs-repl . shadow)
+     (cider-shadow-global-options . "-A:dev")
+     (cider-clojure-cli-global-options . "-A:dev:java9")))
  '(scroll-bar-mode nil)
+ '(sql-mode-hook '(sqlind-minor-mode))
  '(tool-bar-mode nil)
  '(transient-mark-mode nil)
  '(vc-handled-backends nil)
